9476c758dd9194260bedc70b73a6e7b290c80b04
tomo-o30 <tomo.ogawa18@gmail.com>
2022-09-26 19:36:33 +0900

    add: chapter08 httptest_1

---
+++ chapter08/httptest_1/data.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var Db *sql.DB
+
+// connect to the Db
+func init() {
+	var err error
+	Db, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/gwp")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Get a single post
+func retrieve(id int) (post Post, err error) {
+	post = Post{}
+	err = Db.QueryRow("select id, content, author from posts where id = ?", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+// Create a new post
+func (post *Post) create() (err error) {
+	statement := "insert into posts (content, author) values (?, ?)"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+// Update a post
+func (post *Post) update() (err error) {
+	_, err = Db.Exec("update posts set content = ?, author = ? where id = ?", post.Content, post.Author, post.Id)
+	return
+}
+
+// Delete a post
+func (post *Post) delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = ?", post.Id)
+	return
+}
