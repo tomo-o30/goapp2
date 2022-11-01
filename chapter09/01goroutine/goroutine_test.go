72147b9952ddefeff2c38fc9e4170bf4295e7b0e
小川智裕 <tomo.create30@gmail.com>
2022-11-01 21:05:11 +0900

    chapter09: 01goroutine

---
+++ chapter09/01goroutine/goroutine_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestPrint1(t *testing.T) {
+	print1()
+}
+
+func TestGoPrint(t *testing.T) {
+	goPrint1()
+	time.Sleep(1 * time.Millisecond)
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module example.com/m
+
+go 1.19
+
+require golang.org/x/net v0.1.0
+
+require golang.org/x/text v0.4.0 // indirect
