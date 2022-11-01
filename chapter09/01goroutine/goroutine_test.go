6cb6724c773e795920d4f8be71f9faf89c0f6d68
小川智裕 <tomo.create30@gmail.com>
2022-11-01 21:20:49 +0900

    chapter09: 04goroutine

--- chapter09/01goroutine/goroutine_test.go
+++ chapter09/01goroutine/goroutine_test.go
@@ -5,6 +5,18 @@ import (
 	"time"
 )
 
+func BenchmarkPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print1()
+	}
+}
+
+func BenchmarkGoPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint1()
+	}
+}
+
 func TestPrint1(t *testing.T) {
 	print1()
 }
