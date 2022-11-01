6973e75703412a60e96c2dc602d91c874fdf1d53
小川智裕 <tomo.create30@gmail.com>
2022-11-01 21:57:31 +0900

    chapter09: 06goroutine

--- chapter09/01goroutine/goroutine.go
+++ chapter09/01goroutine/goroutine.go
@@ -1,48 +1,32 @@
 package main
 
-// import "fmt"
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-func printNumbers1() {
-	for i := 0; i < 10; i++ {
-		//    fmt.Printf("%d ", i)
-	}
-}
-
-func printLetters1() {
-	for i := 'A'; i < 'A'+10; i++ {
-		//    fmt.Printf("%c ", i)
-	}
-}
-
-func print1() {
-	printNumbers1()
-	printLetters1()
-}
-
-func goPrint1() {
-	go printNumbers1()
-	go printLetters1()
-}
-
-func printNumbers2() {
+func printNumbers2(wg *sync.WaitGroup) {
 	for i := 0; i < 10; i++ {
 		time.Sleep(1 * time.Microsecond)
-		//    fmt.Printf("%d ", i)
+		fmt.Printf("%d ", i)
 	}
+	wg.Done()
 }
 
-func printLetters2() {
+func printLetters2(wg *sync.WaitGroup) {
 	for i := 'A'; i < 'A'+10; i++ {
 		time.Sleep(1 * time.Microsecond)
-		//    fmt.Printf("%c ", i)
+		fmt.Printf("%c ", i)
 	}
-}
-
-func goPrint2() {
-	go printNumbers2()
-	go printLetters2()
+	wg.Done()
 }
 
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go printNumbers2(&wg)
+	go printLetters2(&wg)
+	wg.Wait()
+	fmt.Printf("\n")
 }
